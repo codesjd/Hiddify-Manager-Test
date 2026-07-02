062ca632ef9da34aa355413fa8dcba62b131d6a7
codesjd <thealisajedi@gmail.com>
2026-07-02 18:01:56 +0330

    Hotfix: Revert cache serialization to pickle as json cannot handle SQLAlchemy objects and Enums properly, which broke the panel

--- hiddify-panel/src/hiddifypanel/cache.py
+++ hiddify-panel/src/hiddifypanel/cache.py
@@ -1,14 +1,14 @@
 import os
 from redis_cache import RedisCache, chunks, compact_dump
 import redis
-import json
+from pickle import dumps, loads
 from loguru import logger
 
 redis_client = redis.from_url(os.environ.get("REDIS_URI_MAIN", "redis://127.0.0.1:6379"))
 # print(os.environ["REDIS_URI_MAIN"])
 
 class CustomRedisCache(RedisCache):
-    def __init__(self, redis_client, prefix="rc", serializer=compact_dump, deserializer=json.loads, key_serializer=None, support_cluster=True, exception_handler=None):
+    def __init__(self, redis_client, prefix="rc", serializer=compact_dump, deserializer=loads, key_serializer=None, support_cluster=True, exception_handler=None):
         super().__init__(redis_client, prefix, serializer, deserializer, key_serializer, support_cluster, exception_handler)
         self.cached_functions = set()
 
@@ -33,4 +33,4 @@ class CustomRedisCache(RedisCache):
             return False
 
 
-cache = CustomRedisCache(redis_client=redis_client, prefix="h", serializer=json.dumps, deserializer=json.loads)
+cache = CustomRedisCache(redis_client=redis_client, prefix="h", serializer=dumps, deserializer=loads)
